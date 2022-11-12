b25b45cb37f962d01f4616b0c92f77ef0c537b59
annaknish <108184361+annaknish@users.noreply.github.com>
2022-11-12 15:58:58 +0200

    Added Bootstrap to the project

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
+import React from "react";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Anna Knysh and is {""}
-        <a
-          href="https://github.com/annaknish/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+          <h1>Weather App</h1>
+          <footer>
+            This project was coded by Anna Knysh and is {""}
+            <a
+              href="https://github.com/annaknish/react-weather-app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              open-sourced on GitHub
+            </a>
+          </footer>
+      </div>
     </div>
   );
 }
